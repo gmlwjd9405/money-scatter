a7538d9203e875fac60b506cfbf502d21aa65adb
kwonheejeong <gmlwjd9405@naver.com>
2020-06-26 01:31:51 +0900

    feat: 임의의 랜덤 문자열 token 생성 로직 구현

---
+++ src/main/java/com/kkaopay/money/scatter/token/TokenGenerator.java
@@ -0,0 +1,28 @@
+package com.kkaopay.money.scatter.token;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TokenGenerator {
+
+    public static final int TOKEN_LENGTH = 3;
+    private static final String EMPTY_STRING = "";
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+    public static String generate() {
+        List<String> characters = generateCharacters();
+        Collections.shuffle(characters);
+
+        return characters.stream()
+                .limit(TOKEN_LENGTH)
+                .collect(Collectors.joining(EMPTY_STRING));
+    }
+
+    private static List<String> generateCharacters() {
+        return CHARACTERS.chars()
+                .mapToObj(i -> (char)i)
+                .map(Object::toString)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/kkaopay/money/scatter/token/TokenGeneratorTest.java
@@ -0,0 +1,19 @@
+package com.kkaopay.money.scatter.token;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TokenGeneratorTest {
+
+    @DisplayName("예측 불가능한 3자리 문자열 token 생성")
+    @Test
+    public void generate() {
+        String token = TokenGenerator.generate();
+        System.out.println(token);
+
+        assertThat(token.length())
+                .isEqualTo(TokenGenerator.TOKEN_LENGTH);
+    }
+}
