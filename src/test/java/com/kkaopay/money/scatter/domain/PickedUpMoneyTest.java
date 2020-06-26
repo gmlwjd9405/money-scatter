aed27462d92062b3baebdc0c4d2340d9e0a0f7db
kwonheejeong <gmlwjd9405@naver.com>
2020-06-26 17:34:46 +0900

    test: PickedUpMoneyTest 테스트 코드 추가

---
+++ src/test/java/com/kkaopay/money/scatter/domain/PickedUpMoneyTest.java
@@ -0,0 +1,29 @@
+package com.kkaopay.money.scatter.domain;
+
+import com.kkaopay.money.scatter.fixture.DataFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PickedUpMoneyTest {
+
+    @DisplayName("같은 사용자 id 이면 return true")
+    @ParameterizedTest
+    @MethodSource
+    public void isSameUser(final PickedUpMoney pickedUpMoney, final long expected) {
+        assertThat(pickedUpMoney.isSameUser(expected))
+                .isTrue();
+    }
+
+    private static Stream<Arguments> isSameUser() {
+        return Stream.of(
+                Arguments.of(DataFixture.getPickedUpMoney1(), 222),
+                Arguments.of(DataFixture.getPickedUpMoney2(), 333)
+        );
+    }
+}
--- src/test/java/com/kkaopay/money/scatter/domain/ScatterMoneyTest.java
+++
@@ -1,5 +0,0 @@
-package com.kkaopay.money.scatter.domain;
-
-public class ScatterMoneyTest {
-
-}
