8cf733ffe3679d544be4963d5341e890b2b8372c
kwonheejeong <gmlwjd9405@naver.com>
2020-06-26 17:14:06 +0900

    feat: 뿌리기 머니를 만든 사용자가 맞는지 동일한 방이 맞는지 여부 확인

--- src/main/java/com/kkaopay/money/scatter/pojo/UserAndRoom.java
+++ src/main/java/com/kkaopay/money/scatter/pojo/UserAndRoom.java
@@ -1,9 +1,12 @@
 package com.kkaopay.money.scatter.pojo;
 
 import com.kkaopay.money.scatter.error.exception.NoRequiredHeaderException;
+import lombok.Getter;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Map;
 
+@Getter
 public class UserAndRoom {
 
     public static final String USER_IDENTIFIER_HEADER_NAME = "x-user-id";
@@ -18,10 +21,17 @@ public class UserAndRoom {
     }
 
     public static UserAndRoom of(final Map<String, Object> headers) {
+        validate(headers);
         Long ownerId = Long.parseLong(getValueOrException(headers, USER_IDENTIFIER_HEADER_NAME).toString());
         String roomId = (String) getValueOrException(headers, ROOM_IDENTIFIER_HEADER_NAME);
 
         return new UserAndRoom(ownerId, roomId);
+    }
+
+    private static void validate(final Map<String, Object> headers) {
+        if (CollectionUtils.isEmpty(headers)) {
+            throw new IllegalArgumentException(); // TODO check
+        }
     }
 
     private static Object getValueOrException(final Map<String, Object> headers, final String headerName) {
@@ -31,15 +41,11 @@ public class UserAndRoom {
         throw new NoRequiredHeaderException();
     }
 
-    public boolean isNotSameOwnerId(final Long ownerId) {
-        return !this.ownerId.equals(ownerId);
+    public boolean isSameOwnerId(final Long ownerId) {
+        return this.ownerId.equals(ownerId);
     }
 
-    public Long getOwnerId() {
-        return ownerId;
-    }
-
-    public String getRoomId() {
-        return roomId;
+    public boolean isNotSameRoomId(final String roomId) {
+        return !this.roomId.equals(roomId);
     }
 }
--- src/test/java/com/kkaopay/money/scatter/pojo/UserAndRoomTest.java
+++ src/test/java/com/kkaopay/money/scatter/pojo/UserAndRoomTest.java
@@ -1,18 +1,19 @@
 package com.kkaopay.money.scatter.pojo;
 
 import com.kkaopay.money.scatter.error.exception.NoRequiredHeaderException;
+import com.kkaopay.money.scatter.fixture.DataFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.*;
 
 public class UserAndRoomTest {
 
@@ -25,6 +26,14 @@ public class UserAndRoomTest {
 
         assertThatCode(() -> UserAndRoom.of(headers))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("생성 실패: 필수 헤더가 Null 또는 Empty")
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void createFailureByNullOrEmpty(final Map<String, Object> headers) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> UserAndRoom.of(headers));
     }
 
     @DisplayName("생성 실패: 필수 헤더가 없는 경우 예외 발생")
@@ -46,5 +55,23 @@ public class UserAndRoomTest {
                 Arguments.of(ownerIdHeaderIsNull),
                 Arguments.of(roomIdHeaderIsNull)
         );
+    }
+
+    @DisplayName("뿌리기 머니를 만든 사용자가 맞으면 return true")
+    @Test
+    public void isSameOwnerId()  {
+        UserAndRoom userAndRoom = DataFixture.getUserAndRoom();
+
+        assertThat(userAndRoom.isSameOwnerId((long) 111))
+                .isTrue();
+    }
+
+    @DisplayName("뿌리기 머니를 생성한 방과 일치하지 않으면 return true")
+    @Test
+    public void isNotSameRoomId()  {
+        UserAndRoom userAndRoom = DataFixture.getUserAndRoom();
+
+        assertThat(userAndRoom.isNotSameRoomId("otherRoomId"))
+                .isTrue();
     }
 }
