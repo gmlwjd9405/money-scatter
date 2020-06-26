851e8b619fe18bce9e4861e72cc6dc87a0cf91bd
kwonheejeong <gmlwjd9405@naver.com>
2020-06-26 12:30:44 +0900

    refactor: 받은 머니 정보인  MoneyAndUserDto 구현

---
+++ src/main/java/com/kkaopay/money/scatter/dto/response/MoneyAndUserDto.java
@@ -0,0 +1,35 @@
+package com.kkaopay.money.scatter.dto.response;
+
+import com.kkaopay.money.scatter.domain.PickedUpMoney;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class MoneyAndUserDto {
+
+    private BigDecimal money;
+    private Long userId;
+
+    private MoneyAndUserDto(final BigDecimal money, final Long userId) {
+        this.money = money;
+        this.userId = userId;
+    }
+
+    public static MoneyAndUserDto of(final BigDecimal money, final Long userId) {
+        return new MoneyAndUserDto(money, userId);
+    }
+
+    public static MoneyAndUserDto fromPickedUpMoney(final PickedUpMoney pickedUpMoney) {
+        return MoneyAndUserDto.builder()
+                .money(pickedUpMoney.getMoney())
+                .userId(pickedUpMoney.getUserId())
+                .build();
+    }
+}
---
+++ src/test/java/com/kkaopay/money/scatter/dto/response/MoneyAndUserDtoTest.java
@@ -0,0 +1,34 @@
+package com.kkaopay.money.scatter.dto.response;
+
+import com.kkaopay.money.scatter.fixture.DataFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class MoneyAndUserDtoTest {
+
+    @DisplayName("MoneyAndUserDto 생성")
+    @Test
+    public void create() {
+        assertThatCode(() -> MoneyAndUserDto.of(BigDecimal.valueOf(3000), (long) 222))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("MoneyAndUserDto 로 변환")
+    @Test
+    public void fromPickedUpMoney() {
+        MoneyAndUserDto dto = MoneyAndUserDto.fromPickedUpMoney(DataFixture.getPickedUpMoney());
+
+        MoneyAndUserDto expected = MoneyAndUserDto.builder()
+                .money(BigDecimal.valueOf(3000))
+                .userId((long) 222)
+                .build();
+
+        assertThat(dto)
+                .isEqualTo(expected);
+    }
+}
