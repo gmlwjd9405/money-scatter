c97e90af4e888b20f86aff199d602b0216744a06
kwonheejeong <gmlwjd9405@naver.com>
2020-06-27 15:49:18 +0900

    refactor: apach에서 제공하는 랜덤 문자열 생성 라이브러리로 변경

--- src/main/java/com/kkaopay/money/scatter/support/RandomTokenGenerator.java
+++ src/main/java/com/kkaopay/money/scatter/support/RandomTokenGenerator.java
@@ -1,13 +1,9 @@
 package com.kkaopay.money.scatter.support;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import org.apache.commons.lang3.RandomStringUtils;
 
 public class RandomTokenGenerator implements TokenGenerationStrategy {
 
-    private static final String EMPTY_STRING = "";
-    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 
     private final int length;
 
@@ -21,18 +17,6 @@ public class RandomTokenGenerator implements TokenGenerationStrategy {
 
     @Override
     public String generate() {
-        List<String> characters = generateCharacters();
-        Collections.shuffle(characters);
-
-        return characters.stream()
-                .limit(length)
-                .collect(Collectors.joining(EMPTY_STRING));
-    }
-
-    private List<String> generateCharacters() {
-        return CHARACTERS.chars()
-                .mapToObj(i -> (char)i)
-                .map(Object::toString)
-                .collect(Collectors.toList());
+        return RandomStringUtils.random(length, true, true);
     }
 }
